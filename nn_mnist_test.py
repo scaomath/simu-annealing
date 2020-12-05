4074fba810c762f917809ea205bd5a4600e76a82
S.Cao <scao.math@gmail.com>
2020-12-05 15:14:28 -0600

    Add a simple CNN model

--- nn_mnist_test.py
+++ nn_mnist_test.py
@@ -9,30 +9,12 @@ import torch.optim as optim
 from torch.optim.optimizer import Optimizer
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+from torchsummary import summary
 
+from tqdm import tqdm
 # import Simulated annealing optimizer
 from sa import UniformSampler, GaussianSampler
 #%%
-# Training settings
-# parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-# parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-#                     help='input batch size for training (default: 64)')
-# parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-#                     help='input batch size for testing (default: 1000)')
-# parser.add_argument('--epochs', type=int, default=10, metavar='N',
-#                     help='number of epochs to train (default: 10)')
-# parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-#                     help='learning rate (default: 0.01)')
-# parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-#                     help='SGD momentum (default: 0.5)')
-# parser.add_argument('--no-cuda', action='store_true', default=False,
-#                     help='disables CUDA training')
-# parser.add_argument('--seed', type=int, default=1, metavar='S',
-#                     help='random seed (default: 1)')
-# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-#                     help='how many batches to wait before logging training status')
-# args = parser.parse_args()
-
 args = {'batch_size': 64,
         'test_batch_size': 1000,
         'epochs': 10,
@@ -46,21 +28,26 @@ args = {'batch_size': 64,
 #%%
 args['cuda'] = not args['no_cuda'] and torch.cuda.is_available()
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
 torch.manual_seed(args['seed'])
 if args['cuda']:
     torch.cuda.manual_seed(args['seed'])
 
 #%%
 kwargs = {'num_workers': 1, 'pin_memory': True} if args['cuda'] else {}
+lib_path = "/home/scao/Documents/simu-annealing/"
 train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('./data', train=True, download=True,
+    datasets.MNIST(lib_path+'data', train=True, download=True,
                    transform=transforms.Compose([
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=args['batch_size'], shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('./data', train=False, transform=transforms.Compose([
+    datasets.MNIST(lib_path+'data', train=False, transform=transforms.Compose([
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
@@ -73,8 +60,8 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(320, 64)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -85,9 +72,74 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x)
 
-model = Net()
-if args['cuda']:
-    model.cuda()
+
+
+def conv_block(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                    stride=stride, padding=1, bias=False)
+
+
+# Residual block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv_block(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv_block(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+# ResNet
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv_block(1, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[0], 2)
+        self.layer3 = self.make_layer(block, 64, layers[1], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv_block(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return F.log_softmax(out)
 
 class SimulatedAnnealing(Optimizer):
     def __init__(self, params, sampler, tau0=1.0, anneal_rate=0.0003,
@@ -164,8 +216,8 @@ class SimulatedAnnealing(Optimizer):
         else:
             # evaluate the metropolis criterion
             ap = acceptance_prob(loss, loss_perturbed, tau)
-            print("old = ", loss.item(), "| pert = ", loss_perturbed.item(),
-                  " | ap = ", ap.item(), " | tau = ", tau)
+            # print("old = ", loss.item(), "| pert = ", loss_perturbed.item(),
+            #       " | ap = ", ap.item(), " | tau = ", tau)
             if ap.item() > np.random.rand():
                 return loss_perturbed, True
 
@@ -173,66 +225,93 @@ class SimulatedAnnealing(Optimizer):
             # or if the temp is now annealed
             return loss, False
 
+# %%
+if __name__ == "__main__":
+    
 
-sampler = UniformSampler(minval=-0.5, maxval=0.5, cuda=args['cuda'])
-#sampler = GaussianSampler(mu=0, sigma=1, cuda=args['cuda'])
-optimizer = SimulatedAnnealing(model.parameters(), sampler=sampler)
-#%%
-def train(epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if args['cuda']:
-            data, target = data.cuda(), target.cuda()
+    args = {'batch_size': 64,
+            'test_batch_size': 1000,
+            'epochs': 10,
+            'lr': 0.01,
+            'momentum': 0.9,
+            'no_cuda': False,
+            'seed': 1,
+            'log_interval': 10,
+            'sgd': True,
+            'resnet': True}
+    args['cuda'] = not args['no_cuda'] and torch.cuda.is_available()
+    
+    model = Net()
 
-        data, target = Variable(data), Variable(target)
+    if args['resnet']:
+        net_args = { "block": ResidualBlock,
+                    "layers": [2, 2, 2, 2] }
+        model = ResNet(**net_args)
+    if args['cuda']:
+        model.cuda()
+        
+    datum = next(iter(train_loader))[0]
+    summary(model.cuda(), datum.shape[1:]) # do not feed the batch size
 
-        def closure(weight=None):
-            if torch.is_grad_enabled():
-                optimizer.zero_grad()
+    # sampler = UniformSampler(minval=-0.5, maxval=0.5, cuda=args['cuda'])
+    sampler = GaussianSampler(mu=0, sigma=1, cuda=args['cuda'])
+    optimizer = SimulatedAnnealing(model.parameters(), sampler=sampler)
+    if args['sgd']:
+        optimizer = optim.Adam(model.parameters(), 
+                                lr=1e-3, 
+                                betas=(0.9, 0.99), 
+                                eps=1e-8, 
+                                weight_decay=0) 
 
-            if (weight is not None):
-                state_dict = model.state_dict()
-                keys = list(state_dict.keys())
+    loss_vals = []
+    test_loss_vals = 0.
+    accuracy = 0.
 
-                for key_idx, key in enumerate(keys):
-                    # print(state_dict[key])
-                    # print(weight_new[0]['params'])
-                    state_dict[key] = weight[0]['params'][key_idx]
-                    model.load_state_dict(state_dict)
-            outputs = model(data)
-            loss = F.nll_loss(outputs, target)
-            
-            if loss.requires_grad:
-                loss.backward(retain_graph=True)
-            return loss
+    for epoch in range(1, args['epochs'] + 1):
+        with tqdm(total=len(train_loader)) as pbar:
+            for batch_idx, (data, target) in enumerate(train_loader):
+                data, target = data.cuda(), target.cuda()
+                data, target = Variable(data), Variable(target)
+                outputs = model(data)
+                loss = F.nll_loss(outputs, target)
+                loss_vals.append(loss.detach())
 
-        loss = optimizer.step(closure)
-        if batch_idx % args['log_interval'] == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+                def closure(weight=None):
+                    if torch.is_grad_enabled():
+                        optimizer.zero_grad()
 
-def test():
-    model.eval()
-    test_loss = 0
-    correct = 0
+                    if (weight is not None):
+                        state_dict = model.state_dict()
+                        keys = list(state_dict.keys())
+
+                        for key_idx, key in enumerate(keys):
+                            state_dict[key] = weight[0]['params'][key_idx]
+                            model.load_state_dict(state_dict)
+                    outputs = model(data)
+                    loss = F.nll_loss(outputs, target)
+                    
+                    if loss.requires_grad:
+                        loss.backward(retain_graph=True)
+                    return loss
+
+                optimizer.step(closure)
+                if batch_idx % args['log_interval'] == 0:
+                    desc = f"Epoch [{epoch}/{args['epochs']}]     "
+                    desc += f"[Steps {batch_idx * len(data)} / {len(train_loader.dataset)}]    "
+                    desc += f"Batch Loss Train: {loss.item():.4f}     " 
+                    pbar.set_description(desc)
+                    pbar.update(args['log_interval'])
+    print("Done training.")
+
     for data, target in test_loader:
-        if args['cuda']:
-            data, target = data.cuda(), target.cuda()
+        data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+        test_loss_vals += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-    test_loss /= len(test_loader.dataset)
+        accuracy += pred.eq(target.data.view_as(pred)).cpu().sum()
+    test_loss_vals /= len(test_loader.dataset)    
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-
-for epoch in range(1, args['epochs'] + 1):
-    train(epoch)
-    test()
-
+            test_loss_vals, accuracy, len(test_loader.dataset),
+            100. * accuracy / len(test_loader.dataset)))
 # %%
